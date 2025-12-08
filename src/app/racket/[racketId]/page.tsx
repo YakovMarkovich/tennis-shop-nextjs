b5003e2745f76b71405a28e7227eec1402589fc9
YakovMarkovich <yakob.markovich@gmail.com>
2025-12-08 23:46:00 +0200

    get racket from server instead of generateStaticParams

--- src/app/racket/[racketId]/page.tsx
+++ src/app/racket/[racketId]/page.tsx
@@ -1,37 +1,21 @@
-import styles from "./page.module.css";
-import { rackets } from "@/data/mock";
-
-export const generateStaticParams = () => {
-  return [{ racketId: "1" }, { racketId: "2" }, { racketId: "13" }];
-};
+import { Racket } from "@/components/Racket/page";
+import { getProductById } from "@/services/get-product-byId";
+import notFound from "./not-found";
 
 const RacketPage = async ({ params }: PageProps<"/racket/[racketId]">) => {
   const { racketId } = await params;
 
-  console.log({ racketId });
-  const racket = rackets.find((r) => r.id === Number(racketId));
+  const { isError, data } = await getProductById({ id: racketId });
 
-  if (!racket) {
-    return <div>{`Racket with id ${racketId} not exists`}</div>;
+  if (!isError && !data) {
+    return notFound();
   }
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.left}>
-        <div className={styles.brand}>{racket.brand.name}</div>
-        <div className={styles.name}>{racket.name}</div>
-        <div className={styles.description}>{racket.description}</div>
-      </div>
+  if (isError || !data) {
+    return "error";
+  }
 
-      <div className={styles.center}>
-        <img src={racket.imageUrl} alt={racket.name} className={styles.image} />
-      </div>
-
-      <div className={styles.right}>
-        <div className={styles.price}>${racket.price}</div>
-      </div>
-    </div>
-  );
+  return <Racket racket={data} />;
 };
 
 export default RacketPage;
