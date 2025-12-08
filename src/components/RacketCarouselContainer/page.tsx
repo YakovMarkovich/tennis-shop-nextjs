06f32ebec1b3325b26c239ef235582d243baede0
YakovMarkovich <yakob.markovich@gmail.com>
2025-12-08 23:48:37 +0200

    suspense and data from server

---
+++ src/components/RacketCarouselContainer/page.tsx
@@ -0,0 +1,37 @@
+import RacketCarousel from "../RacketCarousel/page";
+import { IRacket } from "@/app/types/racket";
+
+type Fetcher = (params: { limit: number; page: number }) => Promise<{
+  isError: boolean;
+  data?: IRacket[];
+}>;
+
+type Props = {
+  title: string;
+  link: string;
+  linkText: string;
+  limit: number;
+  page: number;
+  fetcher: Fetcher;
+};
+
+export async function RacketCarouselContainer({
+  title,
+  link,
+  linkText,
+  limit,
+  page,
+  fetcher,
+}: Props) {
+  const response = await fetcher({ limit, page });
+  const items = response.data ?? [];
+
+  return (
+    <RacketCarousel
+      title={title}
+      link={link}
+      linkText={linkText}
+      items={items}
+    />
+  );
+}
